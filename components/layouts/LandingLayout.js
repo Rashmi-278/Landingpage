414929ba68baf0254ed74fb9cbe997a6e19b5eab
Rashmi V A <rashmi.va@renderpub.com>
2021-03-18 22:16:57 +0530

    Added components and Header is working , Need to add header in layout

---
+++ components/layouts/LandingLayout.js
@@ -0,0 +1,4 @@
+import React from "react"
+import {Flex} from "@chakra-ui/react"
+import Header from "../sections/Header"
+import Footer from "../sections/Footer"
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
 
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+
+import theme from '../components/theme/theme'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
